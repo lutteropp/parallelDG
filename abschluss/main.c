ad101643aff70d7e87d23032d5cf834453374f40
GrilledSheep <grilledsheep@googlemail.com>
2015-12-18 02:31:44 +0100

    zeugs hinzugefügt im momment alles ungetestet

--- abschluss/main.c
+++ abschluss/main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 float f1(float x,float y);
 float* createTestMatrix(int h);
-float* jacobi(float * matrix,float* u0,float h, float* x,float*y);
-float f1(float x,float y);
+float* jacobi(float * matrix,float h, float* f);
+float* gausseidel(float * matrix,float h, float* f);
+float* f(float*x,float*y,float h);
+
 
 int main()
 {
-    printf("Hello world!asdf\n");
+printf("Hello world!asdf\n");
+    float h = 2;
+    float* otto= createTestMatrix(h);
+
     return 0;
 }
 float* createTestMatrix(int h)
@@ -32,7 +37,31 @@ float* createTestMatrix(int h)
     return a;
 
 }
-float* jacobi(float * matrix,float* u0,float h, float* x,float*y)
+float* gausseidel(float * matrix,float h, float* f){
+
+    float* a0=matrix;
+    float* a1=matrix;
+    int k =0;
+    int size=1/h;
+    //todo abbruchbedingung
+    for( k =0; k<100000; k++)
+    {
+        a0=a1;
+        int j;
+        for ( j=1; j<size; j++)
+        {
+            int i;
+            for( i=1; i<size; i++)
+            {
+                //todo erinnern was x war
+                a1[i*size + j]=0,25*(a1[i*size + (j-1)] + a1[(i-1)*size + j]+a0[(i+1)*size + j]+a0[i*size + (j+1)]+h*h*f[i*size+j]);
+            }
+        }
+    }
+    return a1;
+}
+
+float* jacobi(float * matrix,float h, float* f)
 {
     float* a0=matrix;
     float* a1=matrix;
@@ -49,11 +78,29 @@ float* jacobi(float * matrix,float* u0,float h, float* x,float*y)
             for( i=1; i<size; i++)
             {
                 //todo erinnern was x war
-                a1[i*size + j]=0,25*(a0[i*size + (j-1)] + a0[(i-1)*size + j]+a0[(i+1)*size + j]+a0[i*size + (j+1)]+h*h*f1(x[i*size + j],y[i*size + j]));
+                a1[i*size + j]=0,25*(a0[i*size + (j-1)] + a0[(i-1)*size + j]+a0[(i+1)*size + j]+a0[i*size + (j+1)]+h*h*f[i*size+j]);
             }
         }
     }
     return a1;
+}
+float* f(float*x,float*y,float h)
+{
+    int size= 1/h;
+    float* tmp=x;
+    int i=0;
+
+    for( i=0; i<size; i++)
+    {
+        int j=0;
+        for(j =0; j<size; j++)
+        {
+        float x1=x[i*size+j];
+        float y1= y[i*size+j];
+            tmp[i*size+j]=f1(x1,y1);
+        }
+    }
+    return tmp;
 }
 float f1(float x,float y)
 {
